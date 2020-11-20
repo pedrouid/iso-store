c31ebda9098e9f61584885d858f577d9d2da529e
Pedro Gomes <pedrogomes94@gmail.com>
2020-11-20 20:19:27 +0100

    dont throw errors

--- src/store.ts
+++ src/store.ts
@@ -12,14 +12,12 @@ export class Store implements IStore {
 
   public async set<T = any>(key: string, value: T): Promise<void> {
     const item = safeJsonStringify(value);
-    if (typeof item !== 'string')
-      throw new Error(`Couldn't stringify value for key: ${key}`);
     this.storage.setItem(key, item);
   }
 
   public async get<T = any>(key: string): Promise<T | undefined> {
     const item = this.storage.getItem(key);
-    if (!item) throw new Error(`Couldn't get value for key: ${key}`);
+    if (item === null) return undefined;
     const value = safeJsonParse(item);
     return value;
   }
--- test/index.test.ts
+++ test/index.test.ts
@@ -6,4 +6,15 @@ describe('IsoStore', () => {
     await store.init();
     expect(store).toBeTruthy();
   });
+  it('set & get', async () => {
+    const key = 'yolo';
+    const value = { data: true };
+    const store = new Store();
+    await store.init();
+    await store.set(key, value);
+    const result = await store.get<typeof value>(key);
+    if (typeof result === 'undefined') throw new Error('result is undefined');
+    expect(result).toBeTruthy();
+    expect(result.data).toEqual(value.data);
+  });
 });
