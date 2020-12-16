401e13d83b20014f586f16f7b250f5117ef755fb
Pedro Gomes <pedrogomes94@gmail.com>
2020-12-16 22:10:44 +0100

    test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,5 @@
 import Store from '../src';
+import MockAsyncStorage from './mock/asyncstorage';
 
 describe('IsoStore', () => {
   it('init', async () => {
@@ -6,10 +7,23 @@ describe('IsoStore', () => {
     await store.init();
     expect(store).toBeTruthy();
   });
-  it('set & get', async () => {
+  it('browser', async () => {
     const key = 'yolo';
     const value = { data: true };
     const store = new Store();
+    await store.init();
+    await store.set(key, value);
+    const result = await store.get<typeof value>(key);
+    if (typeof result === 'undefined') throw new Error('result is undefined');
+    expect(result).toBeTruthy();
+    expect(result.data).toEqual(value.data);
+  });
+  it('react-native', async () => {
+    const key = 'yolo';
+    const value = { data: true };
+    const store = new Store('react-native', {
+      asyncStorage: new MockAsyncStorage(),
+    });
     await store.init();
     await store.set(key, value);
     const result = await store.get<typeof value>(key);
